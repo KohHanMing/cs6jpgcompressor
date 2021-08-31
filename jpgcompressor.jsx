da907c2738bd1a954fd37ed47e4da7affba2917e
KohHanMing <kohhanming1997@gmail.com>
2021-09-01 00:56:14 +0800

    Created code skeleton

--- jpgcompressor.jsx
+++ jpgcompressor.jsx
@@ -9,8 +9,8 @@ function run() {
     //get folder reference
     var folderRef = (new File($.fileName)).parent;
     
-    const sourceFolderParentRef = '/images/uncompressed/pending';
-    const targetFolderRef = '/images/compressed/photoshop/';
+    const sourceFolderParentRef = '/images/uncompressed';
+    const targetFolderRef = '/images/compressed/';
     var sourceFolderRef = folderRef + sourceFolderParentRef; 
     
     //get images
@@ -20,6 +20,9 @@ function run() {
 
     for (var i = 0; i < sourceImages.length; i++) {
         var doc = app.open(sourceImages[i]);
+
+        //Resize image
+        resizeImageForWeb(doc);
 
         var nameSplit = sourceImages[i].name.split(".");
         var path = folderRef + targetFolderRef + nameSplit[0] + '.jpg'
@@ -42,5 +45,26 @@ function saveJpg(document, path) {
     var saveOpts = new JPEGSaveOptions(); //code equivalent of the save options screen
     saveOpts.quality = 0;
     document.saveAs(file, saveOpts, true); //boolean refers to save as copy
+}
+
+function resizeImageForWeb(doc) {
+    var currLayer = doc.artLayers.getByName("Background");
+    doc.activeLayer = currLayer;
+
+    var length = currLayer.bounds[2] - currLayer.bounds[0];
+    var height = currLayer.bounds[3] - currLayer.bounds[1];
+    
+    if (length > 2000 || height > 2000) {
+        if (length > height) {
+            resizeWithLength(doc);
+        } else if (heigth > length) {
+            resizeWithHeight(doc);
+        } else {
+            resizeWithBoth(doc);
+        }
+    }
+    
+
+    doc.resizeImage(length, height, 72, ResampleMethod.BICUBIC);
 }
 
